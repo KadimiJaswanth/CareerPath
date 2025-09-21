c1bc9193dc6fc36a2af0f5ab33cc139219c9a1bb
Kadimi Jaswanth <rishikadimi7@gmail.com>
2025-09-21 11:58:03 +0530

    final push

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "studio-2665875422-9ee7a",
-  "appId": "1:439839363685:web:0ee26eec78f09929992c8c",
-  "apiKey": "AIzaSyDeoJgqm80qsuDp78x2TkFiA0tozVVbkn0",
-  "authDomain": "studio-2665875422-9ee7a.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "439839363685"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
